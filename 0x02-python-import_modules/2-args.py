093c2bfe6beeac02e9f84af25f85a630c06b8b0f
Emebetyw <Emuyoha21@gmail.com>
2022-12-01 17:42:57 +0300

    cli

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 import sys
 n = len(sys.argv) - 1
 if n != 0:
-    print("{} argument:".format(n))
+    print("{} {}:".format(n, "arguments"))
     for i in range(1, n+1):
         print("{}: {}".format(i, sys.argv[i]))
 elif n == 0:
-    print("0 arguments.")
+    print("{} {}".format(0,"arguments"))
