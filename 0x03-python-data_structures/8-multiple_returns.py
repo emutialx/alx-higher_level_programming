4ada21173eefc575fc66f3c2a7ec0a75c172b910
Emebetyw <Emuyoha21@gmail.com>
2022-12-04 11:20:18 +0300

    retrieve length and first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_tuple = tuple(sentence)
-    len_tuple = len(my_tuple)
-    first_char = my_tuple[0]
-    return len_tuple, first_char
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
