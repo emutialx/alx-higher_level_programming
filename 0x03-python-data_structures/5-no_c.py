d3f9f1c144b8be65d9201b89a0e8ed26084c9023
Emebetyw <Emuyoha21@gmail.com>
2022-12-04 08:44:16 +0300

    remove C and c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    l = list(my_string)
-    for x in l:
+    new_str = list(my_string)
+    for x in new_str:
         if x == 'c':
-            l.remove('c')
+            new_str.remove('c')
         if x == 'C':
-            l.remove('C')
+            new_str.remove('C')
     my_string = "".join(l)
     return my_string
