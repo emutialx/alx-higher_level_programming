c31e7fa81d489ead91fe01810754d317ea728103
Emebetyw <Emuyoha21@gmail.com>
2023-01-03 07:25:54 +0300

    unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+     """test using unit test functions for max_integer function"""
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([5, -2, 100, 3]), 100)
+
+    def test_same_max(self):
+        self.assertEqual(max_integer([700, 700, 700]), 700)
+
+    def test_float_max(self):
+        self.assertEqual(max_integer([89.1, 45.78, 70.8]), 89.1)
+
+    def test_neg_max(self):
+        self.assertEqual(max_integer([-10, -5, -2, -1]), -1)
+
+    def test_zero_number(self):
+        self.assertEqual(max_integer([0, 0]), 0)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_operated_integer(self):
+        self.assertEqual(max_integer([-4, -8 * -5, -3, 8]), 40)
+
+    def test_big_list(self):
+        self.assertEqual(max_integer([901, 902, 903, 904, 905, 906, 907, 908, 909, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 920, 919, 918, 917, 1000, 915, 914, 913, 912, 911, 910, 909, 908, 907, 906, 905, 904, 903, 902, 901]), 1000)
+
+    def test_list_with_loop(self):
+        my_list = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer([i * 5 for i in my_list]), 25)
+
+    def test_string_number_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([0, '1'])
+
+     def test_tuple_in_a_list(self):
+         with self.assertRaises(TypeError):
+             max_integer([10, (20, 30)])
+
+    def test_dictionary(self):
+        with self.assertRaises(KeyError):
+            max_integer({'key1': 1, 'key2': 2})
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+                max_integer(1)
