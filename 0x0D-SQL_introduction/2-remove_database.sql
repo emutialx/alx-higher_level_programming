ddc4f0ef50b9a3c4e887be897932fa928e2aa7ec
Emebetyw <Emuyoha21@gmail.com>
2023-02-14 13:34:50 +0300

    first_table created

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---delete a db from mysql server
+-- A script that deletes a db from mysql server
 DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
--- create table with some informations
-CREATE TABLE IF NOT EXIST first_table (id INT, name VARCHAR(256));
+-- a script that creates a table called first_table in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
