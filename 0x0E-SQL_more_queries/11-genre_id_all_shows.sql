7dcb248e932ce769fecf0cc083f6863a4bf662fa
Emebetyw <Emuyoha21@gmail.com>
2023-02-16 17:19:53 +0300

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,8 @@
--- a script that lists all records with a score >= 10 in the table order by score
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
