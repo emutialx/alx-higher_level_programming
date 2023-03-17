d126c115782e0059eb9b135d2e50cfeb9bc521b0
Emebetyw <Emuyoha21@gmail.com>
2023-03-17 09:28:07 +0300

    mysqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
-
+"""
+Lists all states from the database 
+"""
 import MySQLdb
 from sys import argv
 
-'''
-a script that lists all states
-from the database
-'''
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
@@ -17,4 +14,3 @@ if __name__ == "__main__":
     db = cursor.fetchall()
     for i in db:
         print(i)
-    cursor.close()
